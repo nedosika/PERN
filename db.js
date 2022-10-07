4df9a90020aaaa67293a6a4377aa590fd10a15d9
nedosika <nedosika@gmail.com>
2022-10-08 01:07:26 +0300

    add prod dev options

--- db.js
+++ db.js
@@ -3,16 +3,21 @@ import config from "./config.js";
 
 const Pool = pg.Pool;
 
-const devConfig = `postgresql://${config.PG_USER}:${config.PG_PASSWORD}@${config.PG_HOST}:${config.PG_PORT}/${config.PG_DATABASE}`;
+const options = config.NODE_ENV === "production"
+    ? {
+        connectionString: config.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        }
+    }
+    : {
+        connectionString: config.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        }
+        //connectionString: `postgresql://${config.PG_USER}:${config.PG_PASSWORD}@${config.PG_HOST}:${config.PG_PORT}/${config.PG_DATABASE}`
+    }
 
-const proConfig = config.DATABASE_URL;
-
-const pool = new Pool({
-    connectionString:
-        config.NODE_ENV === "production" ? proConfig : devConfig,
-    // ssl: {
-    //     rejectUnauthorized: false,
-    // },
-});
+const pool = new Pool(options);
 
 export default pool;
