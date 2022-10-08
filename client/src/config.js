685bd1f3830ea519f47cced9f27fdf899a02ae14
nedosika <nedosika@gmail.com>
2022-10-08 14:26:20 +0300

    env

--- client/src/config.js
+++ client/src/config.js
@@ -4,10 +4,10 @@ export const CONFIG = {
     NODE_ENV: process.env.NODE_ENV,
     API_URL: process.env.NODE_ENV === 'development'
         ? 'http://localhost:5000'
-        : 'https://test-react-project.herokuapp.com/',
+        : 'https://test-react-project.herokuapp.com',
     WSS_URL: process.env.NODE_ENV === 'development'
         ? 'ws://localhost:5000'
-        : 'ws://test-react-project.herokuapp.com/',
+        : 'wss://test-react-project.herokuapp.com',
     //API_URL: process.env.REACT_APP_API_URL,
    //WSS_URL: process.env.REACT_APP_WSS_URL
 }
