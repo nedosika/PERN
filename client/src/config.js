3e47576424f2acad076fe09d343feb0167d03a87
nedosika <nedosika@gmail.com>
2022-10-08 13:46:15 +0300

    add WSS and API env

--- client/src/config.js
+++ client/src/config.js
@@ -2,6 +2,8 @@ console.log(process.env.NODE_ENV)
 
 export const CONFIG = {
     NODE_ENV: process.env.NODE_ENV,
-    API_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '',
-    WSS_URL: process.env.NODE_ENV === 'development' ? 'ws://localhost:5000' : ''
+    // API_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '',
+    // WSS_URL: process.env.NODE_ENV === 'development' ? 'ws://localhost:5000' : '',
+    API_URL: process.env.API_URL,
+    WSS_URL: process.env.WSS_URL
 }
